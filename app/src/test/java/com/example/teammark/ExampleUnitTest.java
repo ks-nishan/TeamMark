8340457d1aa2296a2e95714512fb8015ab481480
k-nishan <nishakanaga0708@gmail.com>
2021-09-28 19:15:15 +0530

    default unit test

--- app/src/test/java/com/example/teammark/ExampleUnitTest.java
+++ app/src/test/java/com/example/teammark/ExampleUnitTest.java
@@ -16,34 +16,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-
-    private CalculationAD calculationAD;
-    @Before
-    public void setUp() {
-        calculationAD = new CalculationAD();
-    }
-
     @Test
-    public void goldADTest(){
-        int answer = 3000;
-        //int answer = calculationAD.GoldSubTotal(3);
-        assertEquals(3000,answer);
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
-
-    @Test
-    public void silverADTest(){
-        int answer = calculationAD.SilverSubTotal(2);
-        assertEquals(1500,answer);
-    }
-
-    @Test
-    public void brownADTest(){
-        int answer = calculationAD.BrownSubTotal(4);
-        assertEquals(2000,answer);
-    }
-
-    private CalcDuration trip;
-
-
-
 }
